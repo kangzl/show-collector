9ffeb62cddf2d9470023bf19a309b79a3ac31e6e
zhenle.kzl <zhenle.kzl@alibaba-inc.com>
2017-07-10 19:34:35 +0800

    init

---
+++ src/main/java/com/kingfish/show/DataCollecterApplication.java
@@ -0,0 +1,12 @@
+package com.kingfish.show;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCollecterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataCollecterApplication.class, args);
+    }
+}
