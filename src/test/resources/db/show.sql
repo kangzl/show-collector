f3f71660a98d1d9d10daf9e2b60e2a9fd8026ed0
zhenle.kzl <zhenle.kzl@alibaba-inc.com>
2017-07-11 23:48:56 +0800

    backup sql

--- src/test/resources/db/show.sql
+++ src/test/resources/db/show.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50718
  File Encoding         : utf-8
 
- Date: 07/11/2017 11:23:58 AM
+ Date: 07/11/2017 23:48:17 PM
 */
 
 SET NAMES utf8mb4;
@@ -39,6 +39,8 @@ CREATE TABLE `msg` (
   `user_id` bigint(20) unsigned NOT NULL,
   `user_ip` varchar(32) DEFAULT NULL,
   `content` varchar(1024) DEFAULT NULL COMMENT '评论',
+  `product_id` bigint(20) unsigned DEFAULT NULL COMMENT '商品id',
+  `product_key_id` bigint(20) unsigned DEFAULT NULL COMMENT '商品表id',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -47,41 +49,52 @@ CREATE TABLE `msg` (
 -- ----------------------------
 DROP TABLE IF EXISTS `product`;
 CREATE TABLE `product` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `product_id` bigint(20) unsigned NOT NULL,
   `gmt_create` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `gmt_modify` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
-  `url` varchar(2048) DEFAULT NULL,
+  `detail_url` varchar(1024) DEFAULT NULL COMMENT 'detail url',
   `is_p4p` tinyint(1) DEFAULT '0' COMMENT '是否p4p商品',
-  `url_p4p` varchar(2048) DEFAULT NULL COMMENT '广告链接',
+  `p4p_url` varchar(1024) DEFAULT NULL COMMENT '广告链接',
   `cate_id` bigint(20) unsigned DEFAULT NULL COMMENT '类目id',
   `view_sales` varchar(20) DEFAULT NULL COMMENT 'N人收货',
   `sale_id` bigint(20) unsigned DEFAULT NULL COMMENT '卖家id',
-  `l1_category` bigint(20) unsigned DEFAULT NULL COMMENT '一级类目大类'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `l1_category` bigint(20) unsigned DEFAULT NULL COMMENT '一级类目大类',
+  `source` tinyint(4) unsigned DEFAULT NULL COMMENT '来源(0:线下 1:淘宝 2.天猫 3.京东 9.其他)',
+  `pic_url` varchar(1024) DEFAULT NULL COMMENT '图片url',
+  `shop_url` varchar(1024) DEFAULT NULL COMMENT '店铺url',
+  `similar_url` varchar(1024) DEFAULT NULL COMMENT '相似商品的search链接',
+  PRIMARY KEY (`id`),
+  KEY `idx_product_id` (`product_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=121 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Table structure for `show`
+--  Table structure for `shows`
 -- ----------------------------
-DROP TABLE IF EXISTS `show`;
-CREATE TABLE `show` (
+DROP TABLE IF EXISTS `shows`;
+CREATE TABLE `shows` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `gmt_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `gmt_modify` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
+  `content` varchar(1024) DEFAULT NULL COMMENT '内容',
   `truth` double(5,2) DEFAULT '100.00' COMMENT '真实度',
   `praise` int(9) unsigned DEFAULT NULL COMMENT '点赞',
   `product_id` bigint(20) unsigned DEFAULT NULL,
-  `source` tinyint(4) NOT NULL COMMENT '来源',
   `pics` varchar(4096) NOT NULL COMMENT '图片链接,英文逗号分隔',
   `show_time` timestamp NULL DEFAULT NULL COMMENT '评价时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `is_outside` tinyint(1) DEFAULT '0' COMMENT '是否来自外部',
+  `outside_user_head_pic` varchar(256) DEFAULT NULL,
+  `outside_user_nickname` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_product_id` (`product_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2336 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 --  Table structure for `spider_track`
 -- ----------------------------
 DROP TABLE IF EXISTS `spider_track`;
 CREATE TABLE `spider_track` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `gmt_create` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `gmt_modify` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
   `category_id` bigint(20) DEFAULT NULL,
@@ -89,7 +102,7 @@ CREATE TABLE `spider_track` (
   `last_time` timestamp NULL DEFAULT NULL,
   `num` bigint(20) DEFAULT NULL COMMENT '抓取的数量',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 --  Table structure for `user`
@@ -101,11 +114,13 @@ CREATE TABLE `user` (
   `gmt_modify` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
   `username` varchar(256) DEFAULT NULL,
   `password` varchar(512) DEFAULT NULL,
+  `nick_name` varchar(255) DEFAULT NULL,
   `sex` tinyint(4) DEFAULT NULL COMMENT '性别',
   `head_portrait_url` varchar(255) DEFAULT NULL,
   `signature` varchar(1024) DEFAULT NULL COMMENT '签名',
   `mobile_phone_number` varchar(20) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
+  `hide_nick_name` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
